1a888b909e69288d3976a42474ecd504af045c13
Olusola Afikode <afikodeolaitan@gmail.com>
2022-11-21 18:05:59 +0100

    project setup

---
+++ src/views/dashboard/index.jsx
@@ -0,0 +1,10 @@
+import { DashboardRounded } from '@mui/icons-material'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
